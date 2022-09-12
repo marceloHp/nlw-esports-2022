d175c15a27381540ddcbd3dce818c761d2112171
Marcelo Pereira <prog70@ixcsoft.com.br>
2022-09-12 19:04:14 -0300

    feat(test commit):

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Hello worlds!</Text>
       <StatusBar style="auto" />
     </View>
   );
